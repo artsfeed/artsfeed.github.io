93c7d4817ea683c00797bca3bfdb16e88ada0f50
Boubacar Diallo <bouba@meme-machine.local>
2017-11-01 02:31:23 -0400

    js runtime sequence issues from a simple carousel but carousel structure done finally

--- parallax-template/js/init.js
+++ parallax-template/js/init.js
@@ -1,3 +1,4 @@
+
 (function($){
   $(function(){
 
@@ -9,19 +10,98 @@
 
 
 $('.carousel.carousel-slider').carousel({fullWidth: true});
+var int;
+var time = 5000;
+var timeout = 15000
+
+function run(){
+  stop()
+  int = setInterval(function()
+  { 
+      $('.carousel').carousel('next');
+  }, time);
+  console.log(int)
+}
+
+function runback(){
+  stop()
+  int = setInterval(function()
+  { 
+      $('.carousel').carousel('prev');
+  }, time);
+}
+
+function stop(){
+clearInterval(int);
+}
 
 $(document).ready(function(){
-  var carousel_interval = 2500;
-  $('.carousel').carousel();
-  var int;
-  function run(){
-      int = setInterval(function()
-      { 
-          $('.carousel').carousel('next');
-      }, carousel_interval);
-  }
-  function stop(){
-  clearInterval(int);
-  }
-  $('.carousel').hover(stop, run);     
+  run()
+});
+
+$('.carousel').hover(stop, run);
+
+$(".carousel-goleft").click(function(){
+  stop()
+  $('.carousel').carousel('prev');
+  setTimeout(function(){
+    runback()
+  }, timeout);
+});
+
+$(".carousel-goright").click(function(){
+  stop()
+  $('.carousel').carousel('next');
+  setTimeout(function(){
+    run()
+  }, timeout);
+});
+
+
+/*
+  (function($){
+    $(function(){
+  
+      $('.button-collapse').sideNav();
+      $('.parallax').parallax();
+      $('.carousel').carousel();
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
+  
+  
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
+  
+  $(document).ready(function(){
+  
+
+  
+    var carousel_interval = 2500;
+    $('.carousel').carousel();
+    var int;
+    function run(){
+        int = setInterval(function()
+        { 
+            $('.carousel').carousel('next');
+        }, carousel_interval);
+    }
+    
+    function stop(){
+    clearInterval(int);
+    }
+  
+    $('.carousel').hover(stop, run);
+    
+  
   }); 
+  
+  $(".carousel-goleft").click(function(){
+    $('.carousel').carousel('prev');
+  });
+  $(".carousel-goright").click(function(){
+    $('.carousel').carousel('next');
+  });
+  /*
+  $(".carousel-goright").dblclick(function(){
+    $('.carousel').carousel('next', 2);
+  });
+  */
